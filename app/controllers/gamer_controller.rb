08a76a5c8889f40fd5470891024ce6c28bde20b7
Dice-K39 <daisukehashimoto39@gmail.com>
2020-07-25 17:48:52 -0400

    Added messages to let user know of taken usernames and non-matching passwords.

--- app/controllers/gamer_controller.rb
+++ app/controllers/gamer_controller.rb
@@ -4,6 +4,27 @@ class GamerController < ApplicationController
     end
 
     post '/signup' do
+        pw_confirmation = params[:confirm_password]
+        
+        params.delete(:confirm_password)
+
+        gamer = Gamer.new(params)
+        
+
+        if Gamer.find_by(:username => gamer[:username])
+            flash[:username_taken] = "The username is already in use. Please enter a new username or log in to continue."
+
+            redirect to '/signup'
+        else
+            if params[:password] == pw_confirmation
+                Gamer.create(params)
+            else
+                flash[:non_matching_password] = "The password you have entered does not match. Please re-enter your password."
+                
+                redirect to '/signup'
+            end
+        end
+
         erb :'video_games/show'
     end
 end
