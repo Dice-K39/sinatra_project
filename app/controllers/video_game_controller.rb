2473f19e685da13a64a7b9235e9e7994bfadc4f5
Dice-K39 <daisukehashimoto39@gmail.com>
2020-08-01 21:15:48 -0400

    Added to video game controller that user have to be logged in to get, post, patch, or delete entries. Also added required to fields requiring text input.

--- app/controllers/video_game_controller.rb
+++ app/controllers/video_game_controller.rb
@@ -1,49 +1,77 @@
 class VideoGameController < ApplicationController
     get '/video_games' do
-        @gamer = current_gamer
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            @gamer = current_gamer
 
-        erb :'video_games/index'
+            erb :'video_games/index'
+        end
     end
 
     get '/video_games/new' do
-        erb :'video_games/new'
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            erb :'video_games/new'
+        end
     end
 
     post '/video_games' do
-        @video_game = current_gamer.video_games.create(params)
-        
-        if @video_game.save
-            redirect to '/video_games'
-        else
-            redirect to '/video_games/new'
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            @video_game = current_gamer.video_games.create(params)
+            
+            if @video_game.save
+                redirect to '/video_games'
+            else
+                redirect to '/video_games/new'
+            end
         end
     end
 
     get '/video_games/:id' do
-        @video_game = VideoGame.find_by_id(params[:id])
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            @video_game = VideoGame.find_by_id(params[:id])
 
-        erb :'video_games/show'
+            erb :'video_games/show'
+        end
     end
 
     get '/video_games/:id/edit' do
-        @video_game = VideoGame.find_by_id(params[:id])
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            @video_game = VideoGame.find_by_id(params[:id])
 
-        erb :'video_games/edit'
+            erb :'video_games/edit'
+        end
     end
 
     patch '/video_games/:id' do
-        video_game = VideoGame.find_by_id(params[:id])
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            video_game = VideoGame.find_by_id(params[:id])
 
-        params.delete(:_method)
+            params.delete(:_method)
 
-        video_game.update(params)
+            video_game.update(params)
 
-        redirect to '/video_games'
+            redirect to '/video_games'
+        end
     end
 
     delete '/video_games/:id' do
-        VideoGame.delete(params[:id])
-        
-        redirect to '/video_games'
+        if !is_logged_in?
+            redirect to '/login'
+        else        
+            VideoGame.delete(params[:id])
+            
+            redirect to '/video_games'
+        end
     end
 end
